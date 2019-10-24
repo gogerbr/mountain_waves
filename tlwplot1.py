f2d0be15c5caccf362ba9523c7cbb07dd60fa689
gogerbr <brigitta.goger@uibk.ac.at>
2019-10-24 12:00:45 +0200

    almost looks realistic. plot and streamline improvements are still necessary.

--- tlwplot1.py
+++ tlwplot1.py
@@ -34,80 +34,90 @@ import cmath as cm
 #%
 #%--------------------------------------------------------------
 
+Lupper=0.0004;
+Llower=0.1;
+U=20;
+H=3.5;
+a=0.5;
+h0=3;
+xdom=40;
+zdom=10;
+mink=0;
+maxk=30;
 
-Lupper=0.0005
-Llower=0.001
-U=40
-H=3.
-a=2.5
-h0=1
-xdom=40
-zdom=10
-mink=0
-maxk=30
 
 
 def tlwplot(Lupper,Llower,U,H,a,h0,xdom,zdom,mink,maxk):
+	npts = 100;  # cells in each direction
+	dk = 0.367 / a; # % wavenumber interval size (smaller the better, but  need to watch calc.time!)
+	nk = (maxk - mink) / dk;   # loops for wave# integration
+
+	minx = -.25 * xdom; # % leftmost limit of domain
+	maxx = .75 * xdom; #% rightmost limit of domain
+	minz = 0; #% lower limit of domain
+	maxz = zdom; #% upper limit of domain
 
 
-    npts = 100;  # cells in each direction
-    dk = 0.367 / a; # % wavenumber interval size (smaller the better, but  need to watch calc.time!)
-    nk = (maxk - mink) / dk;   # loops for wave# integration
-    minx = -.25 * xdom; # % leftmost limit of domain
-    maxx = .75 * xdom; #% rightmost limit of domain
-    minz = 0; #% lower limit of domain
-    maxz = zdom; #% upper limit of domain
+	matrix1 = np.zeros([npts + 1, npts + 1]); #% temp matrix used in integration
+	matrix2 = np.zeros([npts + 1, npts + 1]); #% temp matrix used in integration
+	matrix3 = np.zeros([npts , npts ]); #% sum matrix used in integration
+	umesh=U*np.ones(matrix3.shape)
+	dx = (maxx - minx) / npts; #% grid cell size in horizontal
+	dz = (maxz - minz) / npts; #% grid cell size in vertical
 
+	x = np.arange(minx, maxx,dx); #% array of x gridpoints
+	z = np.arange(minz, maxz,dz); #% array of z gridpoints
+	k = np.arange(mink, maxk,dk); #% array of wavenumbers
 
-    matrix1 = np.zeros([npts + 1, npts + 1]); #% temp matrix used in integration
-    matrix2 = np.zeros([npts + 1, npts + 1]); #% temp matrix used in integration
-    matrix3 = np.zeros([npts , npts ]); #% sum matrix used in integration
-    umesh=U*np.ones(matrix3.shape)
-    dx = (maxx - minx) / npts; #% grid cell size in horizontal
-    dz = (maxz - minz) / npts; #% grid cell size in vertical
+	ht=0.
+	x,z=np.meshgrid(x,z)
+	nk=np.int(np.round(nk,decimals=0))
 
-    x = np.arange(minx, maxx,dx); #% array of x gridpoints
-    z = np.arange(minz, maxz,dz); #% array of z gridpoints
-    k = np.arange(mink, maxk,dk); #% array of wavenumbers
-    ht=0.
-    x,z=np.meshgrid(x,z)
-#    print(x.shape)
- #   [x, z] = meshgrid(minx:dx: maxx, minz: dz:maxz);
+	for kloop in range(1,nk):
 
-    for kloop in range(1,len(k)):
-        kk = k[kloop]; #% horiz         wavenumber
-      #  print(kk)
-        m = sqrt((Llower * Llower - kk * kk)); #% vert         wave  # in lower layer
+		kk = k[kloop]; #% horiz         wavenumber
+		print(kk)
+		m = sqrt((Llower * Llower - kk * kk)); #% vert         wave  # in lower layer
+		
+		n = sqrt((kk * kk - Lupper * Lupper)); #% vert     wave  # in upper layer
+		print(m,n)
 
-        n = sqrt((kk * kk - Lupper * Lupper)); #% vert     wave  # in upper layer
-        print(m,n)
-        if ((m+n) == 0): # % check for divide by zero
-            r = 9e99;
-        else:
-            r = (m-n) / (m+n); # % reflection coefficient end;
+		if ((m+1j*n) == 0): # % check for divide by zero
+			r = 9e99;
+		else:
+			r = (m-1j*n) / (m+1j*n); # % reflection coefficient end;
 
-        R = r * cm.exp(1j*2* m * H);# % reflection     calculation
-        A = (1 + r) * (cm.exp((H*n+1j*H*m)) / (1 + R));
-        C = 1 / (1 + R);
-        D = R*C;
-        hs =  np.pi*h0*np.exp(-a * np.abs(kk));
-        ht = ht + cm.pi * dk * a * np.exp(-a * np.abs(kk));
+		R = r * cm.exp(1j*2* m * H);# % reflection     calculation
 
-        aboveH = (A * np.exp(-z * n)) ;# % calculate     w in each     layer
-        belowH = C * np.exp(1j*z* m)+D*np.exp(-1j*z*m)
-        aboveidx=np.where((z>H))
-        belowidx=np.where((z<=H))
-        zmat=np.ones([100,100])
-        zmat[aboveidx]=aboveH[aboveidx]*z[aboveidx]
-        zmat[belowidx]=belowH[belowidx]*z[belowidx]
-        matrix2 = zmat*(-1j*kk*hs*U)*np.exp(-1j*x*kk)
-        if kk > 1: #% trapezoidal integration / summation
-            matrix3 = matrix3 + .5 * (matrix1+matrix2) * dk;
-        else:
-            matrix1 = matrix2;
+		A = ((1 + r) * (cm.exp(H*n+1j*H*m))) / (1 + R);
 
-    w = np.real(matrix3/ ht);
-    plt.figure()
+		C = 1 / (1 + R);
+
+		D = R*C;
+		print(dk,'dk')
+		hs =  np.pi*a*h0 *cm.exp(-a * np.abs(kk));
+		ht = ht + np.pi * dk * a * cm.exp(-a * np.abs(kk)); 
+		print(hs,ht,'hsht')
+		aboveH = (A * np.exp(-z * n)) ;# % calculate     w in each     layer
+		belowH = (C * np.exp(1j*z* m)+D*np.exp(-1j*z*m))
+
+		aboveidx=np.where((z>H))
+		belowidx=np.where((z<=H))
+
+		zmat=np.ones(aboveH.shape)
+		zmat[aboveidx]=aboveH[aboveidx]*z[aboveidx]
+		zmat[belowidx]=belowH[belowidx]*z[belowidx]
+		matrix2 = zmat*(-1j*kk*hs*U)*np.exp(-1j*x*kk)
+
+		if kloop > 1: #% trapezoidal integration / summation
+			matrix3 = matrix3 + .5 * (matrix1+matrix2) * dk;
+		else:
+			matrix1 = matrix2;
+
+	w = np.real(matrix3/ ht);
+	plt.figure()
+	plt.clf()
+	plt.close('all')
    # fig = plt.figure(figsize=(1, 1))
    # gs = gridspec.GridSpec(nrows=1, ncols=1, height_ratios=[1, 1, 2])
 
@@ -116,16 +126,15 @@ def tlwplot(Lupper,Llower,U,H,a,h0,xdom,zdom,mink,maxk):
 
     #plt.streamplot(Z[0,100:200],X[:,100:200],U,w)
     #plt.contourf(Z[:,0:100],X[:,0:100],w)
-    Z = np.arange(minz,maxz,dz)
-    X = np.arange(minx,maxx,dx)
-    plt.contourf(x,z,w)
-    plt.streamplot(x,z,umesh,w, density=[0.5, 1],color='k')
-    plt.ylim(0,10)
-    Hline = H * np.ones([1, npts + 1]); #% create    interface     line
-    #plt.plot( Hline, 'm--'); #% draw     interface   line in magenta!
-    plt.show()
-    return (w)
+	Z = np.arange(minz,maxz,dz)
+	X = np.arange(minx,maxx,dx)
+	
+	plt.contourf(x,z,w)
+	plt.streamplot(x,z,umesh,w, density=[0.5, 1],color='k')
+	plt.ylim(0,10)
+	Hline = H * np.ones([1, npts + 1]); #% create    interface     line
+	plt.plot( Hline, 'm--'); #% draw     interface   line in magenta!
+	plt.savefig('mw_fig.png')
+	return (w)
 w=tlwplot(Lupper,Llower,U,H,a,h0,xdom,zdom,mink,maxk)
-#print(func)
-plt.figure()
-#plt.show()
+

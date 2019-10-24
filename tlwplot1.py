e2d4b7cdc27c8531a7b18b449123a2890c04e695
gogerbr <brigitta.goger@uibk.ac.at>
2019-10-24 14:57:56 +0200

    mountain waves are not really realistic. Improve contourplot?

--- tlwplot1.py
+++ tlwplot1.py
@@ -35,25 +35,27 @@ import cmath as cm
 #%--------------------------------------------------------------
 
 Lupper=0.0004;
-Llower=0.1;
-U=20;
+Llower=0.001;
+U=20.;
 H=3.5;
-a=0.5;
-h0=3;
+a=0.71;
+h0=0.5;
 xdom=40;
 zdom=10;
-mink=0;
-maxk=30;
+mink=0.;
+maxk=30.;
+
+
 
 
 
 def tlwplot(Lupper,Llower,U,H,a,h0,xdom,zdom,mink,maxk):
-	npts = 100;  # cells in each direction
+	npts = 1000;  # cells in each direction
 	dk = 0.367 / a; # % wavenumber interval size (smaller the better, but  need to watch calc.time!)
 	nk = (maxk - mink) / dk;   # loops for wave# integration
 
-	minx = -.25 * xdom; # % leftmost limit of domain
-	maxx = .75 * xdom; #% rightmost limit of domain
+	minx = -0.25 * xdom; # % leftmost limit of domain
+	maxx = 0.75 * xdom; #% rightmost limit of domain
 	minz = 0; #% lower limit of domain
 	maxz = zdom; #% upper limit of domain
 
@@ -73,68 +75,65 @@ def tlwplot(Lupper,Llower,U,H,a,h0,xdom,zdom,mink,maxk):
 	x,z=np.meshgrid(x,z)
 	nk=np.int(np.round(nk,decimals=0))
 
+
 	for kloop in range(1,nk):
 
 		kk = k[kloop]; #% horiz         wavenumber
 		print(kk)
-		m = sqrt((Llower * Llower - kk * kk)); #% vert         wave  # in lower layer
+		m = cm.sqrt((Llower * Llower - kk * kk)); #% vert         wave  # in lower layer
 		
-		n = sqrt((kk * kk - Lupper * Lupper)); #% vert     wave  # in upper layer
-		print(m,n)
+		n = cm.sqrt((kk * kk - Lupper * Lupper)); #% vert     wave  # in upper layer
 
 		if ((m+1j*n) == 0): # % check for divide by zero
 			r = 9e99;
 		else:
 			r = (m-1j*n) / (m+1j*n); # % reflection coefficient end;
 
-		R = r * cm.exp(1j*2* m * H);# % reflection     calculation
+		R = r*np.exp(1j*2.*m*H);# % reflection     calculation
 
 		A = ((1 + r) * (cm.exp(H*n+1j*H*m))) / (1 + R);
-
-		C = 1 / (1 + R);
-
+		C = 1 / (1 + R)
 		D = R*C;
-		print(dk,'dk')
-		hs =  np.pi*a*h0 *cm.exp(-a * np.abs(kk));
-		ht = ht + np.pi * dk * a * cm.exp(-a * np.abs(kk)); 
-		print(hs,ht,'hsht')
-		aboveH = (A * np.exp(-z * n)) ;# % calculate     w in each     layer
-		belowH = (C * np.exp(1j*z* m)+D*np.exp(-1j*z*m))
 
-		aboveidx=np.where((z>H))
-		belowidx=np.where((z<=H))
+		hs =  np.pi*a*h0 *np.exp(-a * kk);
+		ht = ht + np.pi * dk * a * np.exp(-a * kk); 
+		print(hs,'hs')
 
-		zmat=np.ones(aboveH.shape)
-		zmat[aboveidx]=aboveH[aboveidx]*z[aboveidx]
-		zmat[belowidx]=belowH[belowidx]*z[belowidx]
-		matrix2 = zmat*(-1j*kk*hs*U)*np.exp(-1j*x*kk)
+	
+		aboveH = (A * np.exp(-z * n))*(z>H) ;# % calculate     w in each     layer
+
+		belowH = (C * np.exp(1j*z*m)+D*np.exp(-1j*m*z))*(z<=H)
+
+
+	  	
+
+		matrix2 = (-1j*U*kk*hs*(aboveH+belowH))*np.exp(kk*-1j*x)
 
 		if kloop > 1: #% trapezoidal integration / summation
-			matrix3 = matrix3 + .5 * (matrix1+matrix2) * dk;
+			matrix3 = matrix3 + .5 * (matrix2) * dk;
+			print(np.min(matrix1),np.min(matrix2),np.min(matrix3))
+			print(np.max(matrix1),np.max(matrix2),np.max(matrix3))
 		else:
 			matrix1 = matrix2;
 
 	w = np.real(matrix3/ ht);
 	plt.figure()
-	plt.clf()
-	plt.close('all')
-   # fig = plt.figure(figsize=(1, 1))
-   # gs = gridspec.GridSpec(nrows=1, ncols=1, height_ratios=[1, 1, 2])
 
-    #  Varying density along a streamline
-   # ax0 = fig.add_subplot(gs[0, 0])
 
-    #plt.streamplot(Z[0,100:200],X[:,100:200],U,w)
-    #plt.contourf(Z[:,0:100],X[:,0:100],w)
 	Z = np.arange(minz,maxz,dz)
 	X = np.arange(minx,maxx,dx)
 	
-	plt.contourf(x,z,w)
-	plt.streamplot(x,z,umesh,w, density=[0.5, 1],color='k')
-	plt.ylim(0,10)
-	Hline = H * np.ones([1, npts + 1]); #% create    interface     line
-	plt.plot( Hline, 'm--'); #% draw     interface   line in magenta!
-	plt.savefig('mw_fig.png')
-	return (w)
-w=tlwplot(Lupper,Llower,U,H,a,h0,xdom,zdom,mink,maxk)
+	plt.contourf(x,z,w,cmap='coolwarm')
+	plt.colorbar()
 
+
+	#Hline = H * np.ones([1, npts + 1]); 
+
+	#plt.plot( Hline, 'm--'); #% draw     interface   line in magenta!
+	plt.savefig('mw_fig.png')
+	plt.close('all')
+
+	return (w)
+
+w=tlwplot(Lupper,Llower,U,H,a,h0,xdom,zdom,mink,maxk)
+print(np.max(w),np.min(w))
